d82129bdb519ab4de028d7fbedca320ee6d758d3
maximus.makarov@mail.ru <4a1AbdPkmh>
2019-08-04 21:26:30 +0300

    3 и 4 задача 2 урока первые 2 задачи в предыдущем коммите

--- Lesson_2/3.py
+++ Lesson_2/3.py
@@ -3,4 +3,26 @@
 цифр и вывести на экран. Например, если введено число 3486,
  то надо вывести число 6843.
 """
+# Без рекурсии
 
+num = int(input('Введите число: '))
+revNum = 0
+
+while num > 0:
+    revNum = revNum * 10 + num % 10
+    num //= 10
+
+print(revNum)
+
+
+# С рекурсией
+
+def reverse(num, revnum=0):
+    if num == 0:
+        return revnum
+    else:
+        return reverse(num // 10, revnum * 10 + num % 10)
+
+
+num = int(input('Введите число: '))
+print(reverse(num))
