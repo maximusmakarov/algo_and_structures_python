8fc804a43ce446687cecd336ea1f0109fa9a07d5
maximus.makarov@mail.ru <4a1AbdPkmh>
2019-08-07 22:25:34 +0300

    1 задача

--- Lesson_1/5.py
+++ Lesson_1/5.py
@@ -1,20 +1,3 @@
 # 5.	Пользователь вводит две буквы. Определить, на каких местах
 # алфавита они стоят, и сколько между ними находится букв.
 
-char1 = 0
-char2 = 0
-
-while char1 == char2 or char1.isdigit() or char2.isdigit():
-    char1 = input('Ведите 1-ю букву: ')
-    char2 = input('Ведите 2-ю букву: ')
-
-    if char1 == char2:
-        print('Введите разные буквы!')
-
-    if char1.isdigit() or char2.isdigit():
-        print('Введите только буквы!')
-
-char1 = ord(char1) - ord('a') + 1
-char2 = ord(char2) - ord('a') + 1
-
-print(f'Введённые буквы стоят на позициях {char1} и {char2}\nБукв между ними {abs(char2 - char1) - 1}')
