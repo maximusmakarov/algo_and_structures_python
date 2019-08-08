637a9c977f854f90ef1e949a0fb5347e76a0ebc8
Максим Макаров <43972571+maximusmakarov@users.noreply.github.com>
2019-08-08 17:43:19 +0300

    8 задача

--- Lesson_3/8.py
+++ Lesson_3/8.py
@@ -4,3 +4,19 @@
 записывать ее в последнюю ячейку строки.
 В конце следует вывести полученную матрицу.
 """
+M = 5
+N = 4
+a = []
+for i in range(N):
+  b = []
+  summ = 0
+  print(f'{i+1}-я строка: ')
+  for j in range(M-1):
+    num = int(input())
+    summ += num
+    b.append(num)
+  b.append(summ)
+  a.append(b)
+
+for i in a:
+  print(i)
