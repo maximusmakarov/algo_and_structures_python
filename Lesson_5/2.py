995b1ab1815742fc513afe359084e8daea882b8c
maximus.makarov@mail.ru <4a1AbdPkmh>
2019-08-17 16:59:28 +0300

    2 задача с применением defaultdict

--- Lesson_5/2.py
+++ Lesson_5/2.py
@@ -5,3 +5,20 @@
 [‘C’, ‘4’, ‘F’] соответственно. Сумма чисел из примера: [‘C’, ‘F’, ‘1’],
 произведение - [‘7’, ‘C’, ‘9’, ‘F’, ‘E’].
 """
+from collections import defaultdict
+from functools import reduce
+
+
+numbers = defaultdict(list)
+
+for i in range(2):
+    ff = input(f'Введите {i+1} -е шестнадцатиричное число: ')
+    numbers[f'{i+1}, {ff}'] = list(ff)
+    print(numbers)
+
+sum_numbers = sum([int(''.join(i), 16) for i in numbers])
+print(f'сумма: ', list(f'{sum_numbers:X}'))
+'''%X - Число в шестнадцатеричной системе счисления (буквы в верхнем регистре).'''
+
+mult_numbers = reduce(lambda x, y: x * y, [int(''.join(i), 16) for i in numbers])
+print(f'произведение: ', list('%X' % mult_numbers))
