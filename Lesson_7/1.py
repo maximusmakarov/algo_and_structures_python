994f7e2cb9cdef4eaede4b03946d67f09e7bfc0c
maximus.makarov@mail.ru <4a1AbdPkmh>
2019-08-22 00:51:10 +0300

    1 задание     - 2 варианта сортировки     - время выполнения этих вариантов при 10000 прогонах     - сравнение эффективности изменений

--- Lesson_7/1.py
+++ Lesson_7/1.py
@@ -5,12 +5,53 @@
 виде функции. По возможности доработайте алгоритм (сделайте его умнее).
 """
 import numpy as np
+from memory_profiler import profile
+from timeit import timeit
+
 lenght = int(input('Введите длину массива: '))
 array = [np.random.randint(-100, 100) for i in range(lenght)]
-print(f'Сгенерированный массив: {array}')
+array1 = array.copy()
 
-for j in range(lenght-1):
-    for k in range(lenght - j - 1):
-        if array[k] > array[k+1]:
-            array[k], array[k+1] = array[k+1], array[k]
-print(f'Отсортированный массив: {array}')
+
+# @profile
+def bb_sort(array):
+    """Стандартный пузырьковый алгоритм"""
+    for j in range(lenght - 1):
+        for k in range(lenght - j - 1):
+            if array[k] > array[k + 1]:
+                array[k], array[k + 1] = array[k + 1], array[k]
+    return array
+
+
+print(f'Сгенерированный массив: {array}')
+new_array = bb_sort(array)
+print(f'Отсортированный массив bb_sort: {new_array}')
+
+
+# @profile
+def bb_sort_faster(array1):
+    """Новый алгоритм сортировки предполагает не делать прогоны сравнения если изменений не состоялось ранее"""
+    change = True  # счетчик изменения массива, по умолчанию True
+    run = lenght - 1  # прогоны измерений элементов
+    while run > 0 and change:  # прогоняет до тех пор пока есть изменения и прогоны > 0
+        change = False  # в начале цикла нет изменений
+        for i in range(run):
+            if array1[i] > array1[i + 1]:
+                change = True  # если условие верно то изменение состоялось и оно True
+                array1[i], array1[i + 1] = array1[i + 1], array1[i]  # меняем элементы местами если верхнее условие True
+        run = run - 1  # сокращаем количество прогонов
+    return array1
+
+
+print(f'Сгенерированный массив: {array1}')
+new_array1 = bb_sort_faster(array1)
+print(f'Отсортированный массив bb_sort1: {new_array1}')
+
+print(timeit('bb_sort(array)', setup='from __main__ import bb_sort, array', number=10000))
+print(timeit('bb_sort_faster(array1)', setup='from __main__ import bb_sort_faster, array1', number=10000))
+
+'''При 100 элементах в массиве и 10000 прогонах этих двух функций получаем время выполнения:
+    bb_sort - 3.08с
+    bb_sort_faster - 0.06с 
+    Новый вариант в 51 раз быстрее стандартного пузырькового алгоритма
+    При использовании профилировщика память тратится одинаково ~ 31 Мб'''
